d8e6d923e4233bee2c01bf65d6faf42f4dd5b7c0
yvvonne <yvonnekajuju6@gmail.com>
2025-11-28 14:39:44 +0300

    finall commit

--- lib/student.py
+++ lib/student.py
@@ -1,8 +1,10 @@
-#!/usr/bin/env python
-
 from user import User
 
 class Student(User):
-    
-    def learn(self):
-        pass
+
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = []
+
+    def learn(self, info):
+        self.knowledge.append(info)
--- lib/teacher.py
+++ lib/teacher.py
@@ -1,10 +1,24 @@
-#!/usr/bin/env python
-
 from user import User
-
 import random
+
+knowledge = [
+    "str is a data type in Python",
+    "programming is hard, but it's worth it",
+    "JavaScript async web request",
+    "Python function call definition",
+    "object-oriented teacher instance",
+    "programming computers hacking learning terminal",
+    "pipenv install pipenv shell",
+    "pytest -x flag to fail fast",
+]
 
 class Teacher(User):
 
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = knowledge
+
     def teach(self):
-        pass
+        # Pick a random index from the list
+        index = random.randint(0, len(self.knowledge) - 1)
+        return self.knowledge[index]
